2b3756a7639f3c425f07d5070519d2a60eb9bb50
Ahmad Mizan N.H <71932983+ahmadmizannh@users.noreply.github.com>
2023-05-14 04:11:06 +0700

    Update Resizy.py

--- Resizy.py
+++ Resizy.py
@@ -23,7 +23,7 @@ def main():
         st.image(image, caption="Original Image")
 
         #Size information
-        st.tittle("Size Information")
+        st.title("Size Information")
         st.subheader("Large (2400x1600)"+", "+"Medium (1200x800)"+", "+"Small (640x480)"+", "+"Custom (Input as you like)")
         # Choose the resize option
         resize_option = st.selectbox("Choose Resize Option", ("Large", "Medium", "Small", "Custom"))
